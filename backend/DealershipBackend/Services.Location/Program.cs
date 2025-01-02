66869f7d13d4a4aa17ef4b8f239c02a1bc585787
スケッチ <54027485+suketti@users.noreply.github.com>
2025-01-02 17:15:49 +0100

    Add untracked files

---
+++ backend/DealershipBackend/Services.Location/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using Services.Location.Context;
+using Services.Location.Mappings;
+
+namespace Services.Location;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        //connectionString = connectionString.Replace("{dbpass}", dbPassword);
+        // Add services to the container.
+
+        builder.Services.AddAutoMapper(typeof(MappingProfile));
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //TODO: When dockerized, make the password an ENV variable. Do not store it in the connection string. (Left like that for DEV purposes as of now)
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
