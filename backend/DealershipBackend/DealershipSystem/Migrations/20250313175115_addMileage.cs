b75b96115fdf84a28ea6717d5d59bcc059fabd07
スケッチ <54027485+suketti@users.noreply.github.com>
2025-03-28 07:57:12 +0100

    Add UserPage and pictures

---
+++ backend/DealershipBackend/DealershipSystem/Migrations/20250313175115_addMileage.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealershipSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class addMileage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Mileage",
+                table: "Cars",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mileage",
+                table: "Cars");
+        }
+    }
+}
